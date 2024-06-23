78449042f3d182b368527785acef455250479974
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-06-23 11:58:25 +0530

    Fixes

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,17 +1,14 @@
-import {initializeApp} from 'firebase/app'
-
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAY9LAlJykObUOxe_JllQcn_kxJM29-glo",
-    authDomain: "eduhub-15130.firebaseapp.com",
-    projectId: "eduhub-15130",
-    storageBucket: "eduhub-15130.appspot.com",
-    messagingSenderId: "697457142685",
-    appId: "1:697457142685:web:5bea87ed08119c6a431d74"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 
 export default app;
